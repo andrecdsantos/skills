7eb60b0c037372ac5f98d659964bd604cd4b5041
André Cardoso dos Santos <andrecdsantos0@gmail.com>
2024-03-06 16:34:49 -0300

    add image background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import { Roboto } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer';
+import Image from 'next/image';
+import backgroundImg from '@/app/assets/background-image-white.jpg'
+
 const mainFontFamily = Roboto({
     subsets: ['latin'],
     weight: ['300', '500', '700', '900'],
@@ -32,8 +35,13 @@ export default function RootLayout({
                 className={`min-h-screen flex flex-col ${mainFontFamily.className}`}
             >
                 <Header />
-                <main className="flex-grow w-full py-4 sm:px-12 xl:px-16 text-center lg:text-start bg-gray-100 text-black">
+                <main className="flex-grow w-full py-4 sm:px-12 xl:px-16 text-center lg:text-start relative text-black">
                     {children}
+                    <Image 
+                        src={backgroundImg} 
+                        alt="imagem de fundo"
+                        className='absolute -z-10 top-0 left-0 h-full'
+                    />
                 </main>
                 <Footer />
             </body>
